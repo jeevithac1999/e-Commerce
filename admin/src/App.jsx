2771683891bf950183ba6c16bfe4b66ec07c00e5
Jeevitha <jeevithac1999@gmail.com>
2025-04-09 10:58:18 +0530

    Navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './Components/NavBar/NavBar'
+import Navbar from './Components/Navbar/Navbar'
 import { Admin } from './Pages/Admin/Admin'
 
 export const App = () => {
